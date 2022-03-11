7c5597a91afee08278e0e40b66bd4ffa68b2447c
Sally-Niasse <79103543+Sally-Jackson@users.noreply.github.com>
2022-03-11 10:05:55 +0100

    api modifié

--- api.php
+++ api.php
@@ -6,12 +6,16 @@ header("Access-Control-Allow-Origin: *");
 header('content-type:application/json');
 
 $bdd=  new PDO('mysql:host=localhost;dbname=musee_veterinaire', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+$sql1= "SELECT * FROM quizz";
+$result= $bdd->query($sql1);
+$result->execute(); 
+echo json_encode($result->fetchAll());
 
-$sth = $dbh->prepare("SELECT nom, couleur FROM fruit");
-$sth->execute();
 
-/* Récupération de toutes les lignes d'un jeu de résultats */
+/* Récupération de toutes les lignes d'un jeu de résultats
 print("Récupération de toutes les lignes d'un jeu de résultats :\n");
 $result = $sth->fetchAll();
 print_r($result);
+*/
+
 ?>
